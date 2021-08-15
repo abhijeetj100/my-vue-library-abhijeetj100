70f81e49d203843a8e1ece3cefc2fd5cba5b3e5c
Abhijeet Jain <abhijeetjain456@gmail.com>
2021-08-16 00:09:34 +0530

    adding button component

---
+++ src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <button @click="click">
+        <slot></slot>
+    </button>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue, Emit } from 'vue-property-decorator'
+
+@Component
+export default class Button extends Vue {
+    @Emit()
+  public click () {}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Banner } from './Banner.vue'
+export { default as Button } from './Button.vue'
 // export Banner;
