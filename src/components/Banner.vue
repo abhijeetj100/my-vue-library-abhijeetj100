b1622dc9c5d548199c025ede8a4008e4364a7fed
Abhijeet Jain <abhijeetjain456@gmail.com>
2021-08-15 22:57:38 +0530

    initial commit

---
+++ src/components/Banner.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="banner" :style="bannerStyles" :class="`banner__${position}`">
+    <slot></slot>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Banner extends Vue {
+  @Prop({ type: String, default: 'top', validator: (position) => ['top', 'bottom'].indexOf(position) > -1 }) public position!: string;
+  @Prop({ type: Object, default: () => {}, validator: (position) => ['top', 'bottom'].indexOf(position) > -1 }) public styles!: Object;
+
+  bannerStyles () {
+    return {
+      ...defaultStyles,
+      ...this.styles
+    }
+  }
+}
+
+const defaultStyles = {
+  left: 0,
+  right: 0
+}
+// export default {
+//   props: {
+//     position: {
+//       type: String,
+//       default: 'top',
+//       validator(position) {
+//         return ['top', 'bottom'].indexOf(position) > -1;
+//       },
+//     },
+//     styles: {
+//       type: Object,
+//       default: () => ({}),
+//     },
+//   },
+//   data() {
+//     return {
+//       bannerStyles: {
+//         ...defaultStyles,
+//         ...this.styles,
+//       },
+//     };
+//   },
+// };
+</script>
+
+<style lang="scss" scoped>
+.banner {
+  padding: 12px;
+  background-color: #fcf6cd;
+  color: #f6a623;
+  text-align: left;
+  position: fixed;
+  z-index: 2;
+}
+.banner__top {
+  top: 0;
+}
+.banner__bottom {
+  bottom: 0;
+}
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="hello">
+    <Banner>Hello, this is Abhijeet</Banner>
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
@@ -35,8 +36,13 @@
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator'
+import Banner from './Banner.vue'
 
-@Component
+@Component({
+  components: {
+    Banner
+  }
+})
 export default class HelloWorld extends Vue {
   @Prop() private msg!: string;
 }
